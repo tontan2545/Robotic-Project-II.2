0ee0321da029dd7e87b4e2bf2da21ebe0104ff44
tontan2545 <tontan2545@gmail.com>
2022-05-12 23:29:46 +0700

    add title to xterm

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-xterm -e bash -c "/usr/bin/python3 /home/rootuser/Desktop/Project-2/detect_serial.py" &
-xterm -e bash -c "sleep 30; screen /dev/ttyACM0 115200" &
+xterm -T "Computer Vision" -e bash -c "/usr/bin/python3 /home/rootuser/Desktop/Project-2/detect_serial.py" &
+xterm -T "Serial Monitor" -e bash -c "sleep 30; screen /dev/ttyACM0 115200" &
 
 
 
